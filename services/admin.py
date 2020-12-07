816440c74d1e2f3d46d33fa1b9667bd9b0bd3449
KimGranaderos <kchangranaderos@gmail.com>
2020-12-07 21:12:59 +0800

    registering the models in admin site

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CoWorkingSpace, Conference, Membership, Customer
 
 # Register your models here.
+admin.site.register(CoWorkingSpace)
+admin.site.register(Conference)
+admin.site.register(Membership)
+admin.site.register(Customer)
