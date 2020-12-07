ef8ba5807986f7c6ea7f213c254782bee1e309d1
KimGranaderos <kchangranaderos@gmail.com>
2020-12-07 21:00:37 +0800

    modified app sales to services

--- sales/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CoWorkingSpace, Customer, Conference, Membership 
-
-admin.site.register(CoWorkingSpace)
-admin.site.register(Customer)
-admin.site.register(Conference)
-admin.site.register(Membership)
